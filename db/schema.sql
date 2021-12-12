abf7ed49d42328cadcbaa96dcec9c6d66ccbbb27
Rae Alejandrino <raealejandrino@gmail.com>
2021-12-12 12:25:35 -0500

    Added drop table statements in schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS department;
 
 
 
@@ -6,7 +9,7 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL
@@ -14,7 +17,7 @@ CREATE TABLE role (
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
